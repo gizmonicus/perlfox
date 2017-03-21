ad1a6e84a15f8593431a4f05d1907fb6fb584312
gizmonicus <watomlinson@gmail.com>
2017-03-21 12:58:50 -0400

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
+
 echo "Adding perlfox-user group with GID $MY_GROUPS"
 groupadd -g $MY_GROUPS perlfox-user
+
 echo "Adding perlfox-user with UID $MY_UID"
 useradd -u $MY_UID -g $MY_GROUPS -s /bin/bash perlfox-user
-
-cp -R /usr/local/share/mozilla_skel /home/perlfox-user/.mozilla
-chown -R perlfox-user:perlfox-user /home/perlfox-user/.mozilla
 
 su - perlfox-user -c firefox
