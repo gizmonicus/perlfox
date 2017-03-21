52f23f2e9d5b2969cbf1662ad1ef4385b40e5f9f
Will Tomlinson <watomlinson@gmail.com>
2017-03-21 14:40:10 -0400

    detect GID with awk; make startup script create .perlfox_home by default

--- run.sh
+++ run.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-echo "Adding perlfox-user group with GID $MY_GROUPS"
-groupadd -g $MY_GROUPS perlfox-user
+echo "Adding perlfox-user group with GID $MY_GID"
+groupadd -g $MY_GID perlfox-user
 
 echo "Adding perlfox-user with UID $MY_UID"
-useradd -u $MY_UID -g $MY_GROUPS -s /bin/bash perlfox-user
+useradd -u $MY_UID -g $MY_GID -s /bin/bash perlfox-user
 
 su - perlfox-user -c firefox
--- start-container.sh
+++ start-container.sh
@@ -1,18 +1,30 @@
 #!/bin/bash
+
+echo -e "Startup:\n---\nCreating homedir for perlfox."
+export PERLFOX_HOME=$HOME/.perlfox_home
+mkdir -p $PERLFOX_HOME
+echo -e "Created directory: $PERLFOX_HOME."
+
+# X11 socket for displaying Firefox UI
 XSOCK=/tmp/.X11-unix
+
+# Xauthority file so X11 will actually trust this container.
 MYXAUTH=$(mktemp /tmp/.docker_xauthXXXXXX)
-touch $MYXAUTH
+echo -e "Configuring xauthority"
 xauth nlist $DISPLAY | sed -e 's/^..../ffff/' | xauth -f $MYXAUTH nmerge -
 
-DOCKER_OPTS="--volume=$XSOCK:$XSOCK:rw
-             --volume=$MYXAUTH:$MYXAUTH:rw
+# In order for home directory integration to work, we need the UID/GID to match
+MY_GID=$(grep ":${UID}:[0-9]*:" /etc/passwd | awk -F: '{print $4}')
+
+echo -e "Detected UID/GID as $UID/$MY_GID"
+
+DOCKER_OPTS="--volume=$XSOCK:$XSOCK
+             --volume=$MYXAUTH:$MYXAUTH
              --env=XAUTHORITY=${MYXAUTH}
              --env=DISPLAY
              --env=MY_UID=$UID
-             --env=MY_GROUPS=$GROUPS"
+             --env=MY_GID=$MY_GID
+             --volume $PERLFOX_HOME:/home/perlfox-user"
 
-if [ -n "$PERLFOX_HOME" ]; then
-    DOCKER_OPTS="$DOCKER_OPTS -v $PERLFOX_HOME:/home/perlfox-user"
-fi
-
+echo -e "Starting Docker:\n---"
 docker run -it $DOCKER_OPTS gizmonicus/perlfox
