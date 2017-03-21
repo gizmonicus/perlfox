85f83463ecdacbed8e890cd55e84d4634ff5929e
Will Tomlinson <watomlinson@gmail.com>
2017-03-21 13:35:12 -0400

    support configuration directory

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,6 @@
 FROM centos:6
-RUN yum -y install firefox java icedtea-web.x86_64
+RUN yum -y update && \
+    yum -y install firefox java icedtea-web.x86_64 && \
+    yum -y clean all
 ADD ./run.sh /usr/local/bin/run.sh
 CMD ["/usr/local/bin/run.sh"]
--- start-container.sh
+++ start-container.sh
@@ -4,11 +4,15 @@ MYXAUTH=$(mktemp /tmp/.docker_xauthXXXXXX)
 touch $MYXAUTH
 xauth nlist $DISPLAY | sed -e 's/^..../ffff/' | xauth -f $MYXAUTH nmerge -
 
-docker run -it \
-        --volume=$XSOCK:$XSOCK:rw \
-        --volume=$MYXAUTH:$MYXAUTH:rw \
-        --env="XAUTHORITY=${MYXAUTH}" \
-        --env="DISPLAY" \
-        --env=MY_UID=$UID \
-        --env=MY_GROUPS=$GROUPS \
-        gizmonicus/perlfox
+DOCKER_OPTS="--volume=$XSOCK:$XSOCK:rw
+             --volume=$MYXAUTH:$MYXAUTH:rw
+             --env=XAUTHORITY=${MYXAUTH}
+             --env=DISPLAY
+             --env=MY_UID=$UID
+             --env=MY_GROUPS=$GROUPS"
+
+if [ -n "$PERLFOX_HOME" ]; then
+    DOCKER_OPTS="$DOCKER_OPTS -v $PERLFOX_HOME:/home/perlfox-user"
+fi
+
+docker run -it $DOCKER_OPTS gizmonicus/perlfox
