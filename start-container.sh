920b3dfd4eaf3c09a02a43a1e1d91222c51e1659
Will Tomlinson <watomlinson@gmail.com>
2017-03-23 16:27:07 -0400

    update start-container script for homedir binding

--- start-container.sh
+++ start-container.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 SSH_KEY=$(mktemp ~/.ssh/perlfox_XXXXXX)
+PF_HOME=$HOME/.perlfox_home
 
 function cleanup() {
     echo "Cleaning up temporary SSH keys"
@@ -16,14 +17,26 @@ function cleanup() {
 # Kill the container on CTRL+C
 trap cleanup INT
 
+# Create a homedir for perlfox
+mkdir -p $PF_HOME
+
 # Create the SSH key for automatic login
 echo -e "y\n" | ssh-keygen -t rsa -N "" -f $SSH_KEY >/dev/null || exit 1
 PF_KEY="$(cat ${SSH_KEY}.pub)"
 
+# In order for home directory integration to work, we need the UID/GID to match
+MY_GID=$(grep ":${UID}:[0-9]*:" /etc/passwd | awk -F: '{print $4}')
+
 # Run docker container
-echo "Starting Docker"
+echo "Pulling and running docker container"
 docker pull gizmonicus/perlfox:testing
-docker run -d -p 2022:22 -e "PF_KEY=$PF_KEY" --name perlfox-session gizmonicus/perlfox:testing >/dev/null
+docker run -d -p 2022:22 \
+        -e "MY_GID=$MY_GID" \
+        -e "UID=$UID" \
+        -e "PF_KEY=$PF_KEY" \
+        -v "$PF_HOME:/home/perlfox-user" \
+        --name perlfox-session \
+    gizmonicus/perlfox:testing >/dev/null
 
 # Wait for SSH to start accepting connections
 REPEAT='true'
