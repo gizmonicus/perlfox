4e2861883c375bd4c9c946921b04deccc22af9c2
Will Tomlinson <watomlinson@gmail.com>
2017-03-23 17:02:57 -0400

    polishing up start-container

--- start-container.sh
+++ start-container.sh
@@ -21,7 +21,7 @@ trap cleanup INT
 mkdir -p $PF_HOME
 
 # Create the SSH key for automatic login
-echo -e "y\n" | ssh-keygen -t rsa -N "" -f $SSH_KEY >/dev/null || exit 1
+echo -e "y\n" | ssh-keygen -t rsa -N "" -f $SSH_KEY || exit 1
 PF_KEY="$(cat ${SSH_KEY}.pub)"
 
 # In order for home directory integration to work, we need the UID/GID to match
@@ -32,16 +32,26 @@ echo "Pulling and running docker container"
 docker pull gizmonicus/perlfox:testing
 docker run -d -p 2022:22 \
         -e "MY_GID=$MY_GID" \
-        -e "UID=$UID" \
+        -e "MY_UID=$UID" \
         -e "PF_KEY=$PF_KEY" \
         -v "$PF_HOME:/home/perlfox-user" \
         --name perlfox-session \
-    gizmonicus/perlfox:testing >/dev/null
+    gizmonicus/perlfox:testing
 
 # Wait for SSH to start accepting connections
 REPEAT='true'
+COUNT='0'
+LIMIT='5'
 while "$REPEAT"; do
     ssh -X -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -p 2022 -i $SSH_KEY perlfox-user@localhost 2>/dev/null && REPEAT='false'
+
+    # Try for $LIMIT seconds
+    COUNT=$[COUNT + 1]
+    sleep 1
+    if [ "$COUNT" -ge "$LIMIT" ]; then
+        echo "Error connecting to SSH daemon after $COUNT attempts"
+        break
+    fi
 done
 
 # Clean up after ourselves
