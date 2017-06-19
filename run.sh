b2d455b4a22a1226dfd60c4ce5687b59099f225b
Will Tomlinson <watomlinson@gmail.com>
2017-06-19 13:23:10 -0400

    clean up run.sh script, chown once, and set sshd options on same line

--- run.sh
+++ run.sh
@@ -12,18 +12,17 @@ useradd -u $MY_UID -g $MY_GID -d $PF_HOME perlfox-user
 mkdir -p /home/perlfox-user/.ssh
 echo $PF_KEY > $PF_HOME/.ssh/authorized_keys
 chmod 700 $PF_HOME/.ssh && chmod 400 $PF_HOME/.ssh/authorized_keys && chmod 700 $PF_HOME
-chown -R perlfox-user:perlfox-user $PF_HOME/.ssh
 
 # Configure .bashrc, if one doesn't exist
 test -f $PF_HOME/.bashrc || \
-    cp /etc/skel/.bashrc $PF_HOME/.bashrc && \
-    chown perlfox-user:perlfox-user $PF_HOME/.bashrc
+    cp /etc/skel/.bashrc $PF_HOME/.bashrc
 
 # Configure .bash_profile, if one doesn't exist
 test -f $PF_HOME/.bash_profile || \
-    cp /etc/skel/.bash_profile $PF_HOME/.bash_profile && \
-    chown perlfox-user:perlfox-user $PF_HOME/.bash_profile
+    cp /etc/skel/.bash_profile $PF_HOME/.bash_profile
 
-export OPTIONS="-D"
+# Set perlfox-user as homedir owner
+chown -R perlfox-user:perlfox-user $PF_HOME
+
 echo "Starting SSH Server; press Ctrl + C to stop."
-/etc/init.d/sshd start >/dev/null
+OPTIONS="-D" /etc/init.d/sshd start >/dev/null
